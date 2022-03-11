9889da5ebb14458eb0ad99c0992b9369df34e879
root <root@DESKTOP-KUTNPVK.localdomain>
2022-03-11 14:14:35 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+lines (17 sloc)  212 Bytes
+   
+#include <stdio.h>
+
+/**
+* main - prints alphabet
+* Description: alphabet in lowercase
+* Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
