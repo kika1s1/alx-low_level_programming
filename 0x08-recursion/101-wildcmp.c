5b4ae48ec063084c37290263952296f4f4162dde
root <root@DESKTOP-KUTNPVK.localdomain>
2022-03-30 15:11:55 +0300

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,42 +1,29 @@
 #include "main.h"
-#include "stdio.h"
-#include "stdlib.h"
 
 /**
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: string to compare to
+ * @s2: string with wild character
  *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (1 + _strlen_recursion(s));
-}
-
-/**
- *
- */
-
 int wildcmp(char *s1, char *s2)
 {
-	len = _strlen_recursion(s1);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-	return (tmp(s1, s2, len));
-}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-/**
- *
- */
-
-int tmp(char *s1, char *s2, len)
-{
-	if (*s1 != '\0' && *s2 != '\0')
+	if (*s2 == '*')
 	{
-		if (*s1 == *s2)
-			return (tmp(s1 + 1, s2 +  1));
-		else
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
 	}
-	return (1)
+
+	return (0);
 }
